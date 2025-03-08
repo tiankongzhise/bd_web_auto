3c6dd7ddfe5006c05106aa3fcd3e19e0cb921656
hbc_thinkbook <419337244@qq.com>
2025-03-08 11:17:33 +0800

    实现了一跳创建

--- test/temp.py
+++ test/temp.py
@@ -2,7 +2,5 @@ import tomllib
 import os
 
 if __name__ == '__main__':
-    config_path = os.path.abspath('config.toml')
-    with open(config_path, "rb") as f:
-            config = tomllib.load(f)
-    print([config['user_map']['金蛛-新账户4']])
+    config_path = os.path.('https:\\jmy-pic.baidu.com\\0\\pic\\915332669_-2084204044_-569106030.png')
+    print(config_path)
